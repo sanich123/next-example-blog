dfa7f5144ea3e121d5679e5f5ca68eae7c114ea3
sanich123 <aorovonin.piano@gmail.com>
2022-04-19 19:36:28 +0300

    Some work with Next repo

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
-    return (
-    <>
-    <h1>First Post</h1>
-      <Link href='/'>
-        <a>
-          <h2>I decide to go home!</h2>
-        </a>
-      </Link>
-    </>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
 }
